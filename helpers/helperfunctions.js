56d0cf8d68e98ac01cd29231432c0ac12044c2a3
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-10-19 10:52:30 -0400

    added helper functions to query database based on the users response to inquirer prompt on server.js

--- helpers/helperfunctions.js
+++ helpers/helperfunctions.js
@@ -2,6 +2,11 @@ const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -13,19 +18,74 @@ const db = mysql.createConnection(
 );
 
 const viewDepartments = () => {
+    app.get('/api/departments', (req, res) => {
+        const sql = `SHOW * FROM department;`;
 
+        db.query(sql, (err, rows) => {
+            if(err) {
+                res.json(err);
+                return;
+            } else {
+                res.json({
+                    message: 'Departments',
+                    data: rows
+                });
+            }
+        });
+    });
 };
 
 const viewRoles = () => {
+    app.get('/api/roles', (req, res) => {
+        const sql = `SHOW * FROM role;`;
 
+        db.query(sql, (err, rows) => {
+            if(err) {
+                res.json(err);
+                return;
+            } else {
+                res.json({
+                    message: 'Roles',
+                    data: rows
+                });
+            }
+        });
+    });
 };
 
 const viewEmployees = () => {
+    app.get('/api/employees', (req, res) => {
+        const sql = `SHOW * FROM employee;`;
 
+        db.query(sql, (err, rows) => {
+            if(err) {
+                res.json(err);
+                return;
+            } else {
+                res.json({
+                    message: 'Employees',
+                    data: rows
+                });
+            }
+        });
+    });
 };
 
 const addDepartment = () => {
+    app.post('/api/new-department', (req ,res) => {
+        const sql = `INSERT INTO department (id, name)
+        VALUES (?)`;
+        const params = [req.body.id, req.body.name];
 
+        db.query(sql, params, (err,result) => {
+            if(err) {
+                res.json(err);
+                return;
+            } else {
+                res.json('Successfully added a new department');
+            }
+        });
+    });
 };
 
 const addRole = () => {
